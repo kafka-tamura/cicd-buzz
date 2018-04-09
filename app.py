03bbdb42a5f1367be7b01e5ba4a8e9ce534c1baa
Ashwinikumar Patil <kafka-tamura@outlook.com>
2018-04-10 02:23:28 +0530

    template not found fix 2

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ import signal
 from flask import Flask, render_template
 from buzz import generator
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./templates/')
 
 signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 
 @app.route("/")
 def generate_buzz():
     buzz = generator.generate_buzz()
-    return render_template('templates/index.html', buzz=buzz)
+    return render_template('index.html', buzz=buzz)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
