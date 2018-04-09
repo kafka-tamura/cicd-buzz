db14363fec51844f9236154e35a375604ed7300d
Ashwinikumar Patil <kafka-tamura@outlook.com>
2018-04-10 02:02:14 +0530

    dockerhub hack 3

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,8 +5,8 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
 REPO_NAME=`echo $TRAVIS_REPO_SLUG | cut -d'/' -f2`
 echo $REPO_NAME
-docker push $DOCKER_USER/$REPO_NAME
+docker build -f Dockerfile -t $DOCKER_USER/$REPO_NAME:$TAG .
+docker push $DOCKER_USER/$REPO_NAME:$TAG
 # docker push $TRAVIS_REPO_SLUG
