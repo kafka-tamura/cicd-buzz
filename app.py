40a2bab5eb372262b064f793ba47e7f75093c0a6
Ashwinikumar Patil <kafka-tamura@outlook.com>
2018-04-10 02:43:08 +0530

    fix 5

--- app.py
+++ app.py
@@ -3,16 +3,13 @@ import signal
 from flask import Flask, render_template
 from buzz import generator
 
-app = Flask(__name__, template_folder='src/templates/')
+app = Flask(__name__)
 
 signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 
 @app.route("/")
 def generate_buzz():
     buzz = generator.generate_buzz()
-    print(os.getcwd())
-    print(os.listdir('./'))
-    print(os.path.realpath(__file__))
     return render_template('index.html', buzz=buzz)
 
 if __name__ == "__main__":
