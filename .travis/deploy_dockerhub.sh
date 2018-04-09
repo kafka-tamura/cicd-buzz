bf5307d128456d38482c118454797c71e5a99a3d
Ashwinikumar Patil <kafka-tamura@outlook.com>
2018-04-10 01:30:37 +0530

    debug dockerfile

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,5 +5,9 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
+echo "Building docker container"
+echo $TRAVIS_REPO_SLUG
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+echo "Pushing to docker"
+echo $TRAVIS_REPO_SLUG
 docker push $TRAVIS_REPO_SLUG
